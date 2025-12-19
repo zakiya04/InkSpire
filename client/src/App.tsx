a30afc0747ea1cd4ca103f2c90838d004167853a
Zakiya <zakiyamanzoor04@gmail.com>
2025-12-19 21:36:18 +0530

    add routing for pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProtectedRoute from "./pages/protected";
+import Home from "./pages/home";
+import SearchPage from "./pages/search";
+import Post from "./pages/post";
+import NotFound from "./pages/notFound";
 
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<ProtectedRoute />,
+    children:[
+      {
+        index: true,
+        element:<Home/>
+      },
+      {
+        path: "search",
+        element: <SearchPage />,
+      },
+      { path: "*",
+       element: <NotFound /> 
+      },
+      {
+        path: "posts/:id",
+        element: <Post />,
+      },
+    ]
+  }
+]);
 function App() {
   
 
   return (
     <>
-      <h1 className="bg-blue-700">Hello</h1>
+       <RouterProvider router={router} />
     </>
   )
 }
